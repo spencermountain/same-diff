4c5af15478b425a496762bd43044c1f7e6946a7e
spencer kelly <spencerkelly86@gmail.com>
2026-01-11 13:32:59 -0500

    first build

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+const name = 'same-diff'
+
+import pkg from './package.json' with { type: 'json' }
+
+const banner = `/* ${name} ${pkg.version} MIT */`
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: `builds/${name}.js`,
+        format: 'esm',
+        banner: banner,
+      },
+    ],
+    plugins: [resolve(), json(), commonjs(), terser()],
+  },
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: `builds/${name}.min.js`,
+        format: 'umd',
+        name,
+        banner: banner,
+      },
+    ],
+    plugins: [resolve(), json(), commonjs(), terser()],
+  },
+]
